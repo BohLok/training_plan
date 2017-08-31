b0088367933a98cc5da914ea1d37d21404e2bbb1
JanMeckelholt <Jan_Meckelholt@web.de>
2017-08-31 06:26:01 +0200

    User show, edit, destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,15 +9,41 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update!(user_params)
+      redirect_to @user, alert: "Update successfull!"
+    else
+      render "edit"
+      flash.now "Could not update!"
+    end
+  end
+
+
   def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy!
+      redirect_to users_path, alert: "User deleted!"
+    else
+      render "index"
+      flash.now "Could not delete User!"
+    end
   end
 
 private
 
-def authenticate_admin
-    redirect_to root_path, alert: 'Not authorized.' unless current_user && current_user.admin?
-end
+  def authenticate_admin
+      redirect_to root_path, alert: 'Not authorized.' unless current_user && current_user.admin?
+  end
 
+  def user_params
+    params.require(:user).permit(:email)
+  end
 end
