08ab916a3cb91b96537dceaf8c9744adb50cf0c7
JanMeckelholt <Jan_Meckelholt@web.de>
2017-09-13 06:04:55 +0200

    weeksum function - untested yet

--- app/models/training.rb
+++ app/models/training.rb
@@ -7,4 +7,20 @@ class Training < ApplicationRecord
     self.t_date
   end
 
+  def date_monday(date)
+    date = date.is_a? String ? Date.parse date : date 
+    date = date - date.wday + 1
+  end
+
+  def weeksum(date, trainee_id)
+    weeksum = 0
+    trainee_traing = self.find_by_user_id(trainee_id)
+    date_monday = date_monday(date)
+    for d in date_monday..date_monday+7
+      trainee_traing.find_by_t_date(d).each do |t|
+        weeksum += t.length
+      end
+    end
+    weeksum
+  end
 end
