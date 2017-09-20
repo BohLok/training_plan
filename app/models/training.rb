903a1530bd9d85d40ddf1e4adbd2725dfae83d83
JanMeckelholt <Jan_Meckelholt@web.de>
2017-09-20 06:32:30 +0200

    fixed weeksum

--- app/models/training.rb
+++ app/models/training.rb
@@ -7,18 +7,27 @@ class Training < ApplicationRecord
     self.t_date
   end
 
-  def date_monday(date)
-    date = date.is_a? String ? Date.parse date : date 
+  def self.date_monday(date)
+    # date = date.is_a? String ? Date.parse(date) : date 
+    if date.is_a? String
+      date = Date.parse(date)
+    elsif !date.is_a? Date 
+      date = Date.parse('1.1.1900')
+    end
     date = date - date.wday + 1
   end
 
-  def weeksum(date, trainee_id)
+  def self.weeksum(date, trainee_id)
     weeksum = 0
-    trainee_traing = self.find_by_user_id(trainee_id)
+    trainee_trainings = self.where(user_id: trainee_id)
+    
     date_monday = date_monday(date)
     for d in date_monday..date_monday+7
-      trainee_traing.find_by_t_date(d).each do |t|
-        weeksum += t.length
+      if trainee_trainings.where(t_date: d)
+
+        trainee_trainings.where(t_date: d).each do |t|
+          weeksum += t.length
+        end
       end
     end
     weeksum
