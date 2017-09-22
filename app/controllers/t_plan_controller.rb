5dd4170bf75ace9c9b0e1554c5f17a5d5b52ea0e
JanMeckelholt <Jan_Meckelholt@web.de>
2017-09-22 06:10:11 +0200

    params :start_date added to controller

--- app/controllers/t_plan_controller.rb
+++ app/controllers/t_plan_controller.rb
@@ -1,6 +1,7 @@
 class TPlanController < ApplicationController
  before_action :authenticate_user!
  before_action :set_trainee
+ before_action :set_start_date
   
   def show_month
     @trainings = @trainee.trainings
@@ -17,6 +18,13 @@ private
     else
       @trainee = current_user
     end
+  end
+
+  def set_start_date
+    if !@start_date
+      @start_date = Date.today
+    end
+    params[:start_date] = @start_date
   end
 
 end
